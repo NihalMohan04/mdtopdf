4368f5bfe0e3030b39233916f01b46bac7371658
NihalMohan04 <nihalmohan46@gmail.com>
2026-03-30 19:48:57 +0530

    Add Preview component with GFM support and syntax highlighting

---
+++ src/components/Preview.jsx
@@ -0,0 +1,20 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeHighlight from 'rehype-highlight';
+import 'highlight.js/styles/github.css';
+import './Preview.css';
+
+function Preview({ content }) {
+  return (
+    <div className="preview">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeHighlight]}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
+  );
+}
+
+export { Preview };
